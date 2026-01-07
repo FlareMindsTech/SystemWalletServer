d019ae9ee2135c5f560f8930e26fe1f3223f46ad
vishnu <vishnuak@codiis.com>
2026-01-07 14:54:36 +0530

    wallet

---
+++ Controller/WalletTransaction.js
@@ -0,0 +1,56 @@
+export const addMoney = async (req, res) => {
+  const { userId, amount } = req.body;
+
+  if (amount <= 0) {
+    return res.status(400).json({ message: "Invalid amount" });
+  }
+
+  const user = await User.findById(userId);
+  if (!user) return res.status(404).json({ message: "User not found" });
+
+  user.walletBalance += Number(amount);
+  await user.save();
+
+  await WalletTransaction.create({
+    userId,
+    action: "ADD",
+    amount,
+    balanceAfter: user.walletBalance
+  });
+
+  res.json({
+    message: "Money added successfully",
+    balance: user.walletBalance
+  });
+};
+
+
+export const deductMoney = async (req, res) => {
+  const { userId, amount } = req.body;
+
+  if (amount <= 0) {
+    return res.status(400).json({ message: "Invalid amount" });
+  }
+
+  const user = await User.findById(userId);
+  if (!user) return res.status(404).json({ message: "User not found" });
+
+  if (user.walletBalance < amount) {
+    return res.status(400).json({ message: "Insufficient balance" });
+  }
+
+  user.walletBalance -= Number(amount);
+  await user.save();
+
+  await WalletTransaction.create({
+    userId,
+    action: "DEDUCT",
+    amount,
+    balanceAfter: user.walletBalance
+  });
+
+  res.json({
+    message: "Money deducted successfully",
+    balance: user.walletBalance
+  });
+};
---
+++ Modules/WalletTransactionModule.js
@@ -0,0 +1,30 @@
+import mongoose from "mongoose";
+const walletTransaction = new mongoose.Schema(
+  {
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true
+    },
+
+    action: {
+      type: String,
+      enum: ["ADD", "DEDUCT"],
+      required: true
+    },
+
+    amount: {
+      type: Number,
+      required: true,
+      min: 1
+    },
+
+    balanceAfter: {
+      type: Number,
+      required: true
+    }
+  },
+  { timestamps: true }
+);
+const WalletTransaction=mongoose.model('WalletTransaction',walletTransaction)
+export default WalletTransaction;
